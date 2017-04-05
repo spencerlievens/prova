1e51613bf08ee6df8351f353e2e389fec553d4f2
Ben Davenport <ben@bitgo.com>
2017-04-05 11:29:41 -0700

    Patch commit e320330d29cd7c3383129bf1ed2fb7e46f588a80 from btcd Author: Dave Collins <davec@conformal.com> Date:   Wed Nov 2 16:17:50 2016 -0500
        multi: Remove unused code found by deadcode.

--- blockmanager.go
+++ blockmanager.go
@@ -22,8 +22,6 @@ import (
 )
 
 const (
-	chanBufferSize = 50
-
 	// blockDbNamePrefix is the prefix for the block database name.  The
 	// database type is appended to this value to form the full block
 	// database name.
