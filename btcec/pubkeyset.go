459ce119560702928695bcaa070c4da8f00c2ea2
Alexander Bosworth <alexbosworth@bitgo.com>
2017-04-06 16:23:03 -0700

    btcec: Add test to pubkeyset, expand documentation
    Summary: Put up a test file for pubkeyset. This also removes a panic when parsing pub key strings, which maybe should be replaced, but closer to the strings in question'
    
    Reviewers: johann, ben
    
    Reviewed By: ben
    
    Differential Revision: https://phabricator.bitgo.com/D5065

--- btcec/pubkeyset.go
+++ btcec/pubkeyset.go
@@ -18,6 +18,7 @@ const (
 	ASPKeySet       KeySetType = 4
 )
 
+// String returns the key set type as a human-readable string.
 func (kstype KeySetType) String() string {
 	switch kstype {
 	case RootKeySet:
@@ -36,12 +37,16 @@ func (kstype KeySetType) String() string {
 }
 
 // ParsePubKeySet parses a list of ecdsa.Publickey for a koblitz curve from a
-// list of hex encoded strings, verifying that it is valid.
+// list of hex encoded strings, verifying that they are valid.
 func ParsePubKeySet(curve *KoblitzCurve, pubKeys ...string) (PublicKeySet, error) {
 	keys := make([]PublicKey, len(pubKeys))
 
 	for i, pubKeyStr := range pubKeys {
-		key, err := ParsePubKey(hexToBytes(pubKeyStr), curve)
+		keyBytes, err := hex.DecodeString(pubKeyStr)
+		if err != nil {
+			return nil, err
+		}
+		key, err := ParsePubKey(keyBytes, curve)
 		if err != nil {
 			return nil, err
 		}
@@ -60,25 +65,12 @@ func DeepCopy(adminKeySets map[KeySetType]PublicKeySet) map[KeySetType]PublicKey
 	return copiedMap
 }
 
-// hexToBytes converts the passed hex string into bytes and will panic if there
-// is an error.  This is only provided for the hard-coded constants so errors in
-// the source code can be detected. It will only (and must only) be called with
-// hard-coded values.
-func hexToBytes(s string) []byte {
-	b, err := hex.DecodeString(s)
-	if err != nil {
-		panic("invalid hex in source file: " + s)
-	}
-	return b
-}
-
-// PublicKeySet is a Set of ecdsa.PublicKey with basic functions to
-// add, remove, serialize.
+// PublicKeySet is a set of ecdsa.PublicKey with basic functions to add,
+// remove, and serialize.
 type PublicKeySet []PublicKey
 
-// Pos returns the position of a public key in keyset.
-// -1 is returned if element not found.
-// This is a basic collection operation, golang should have it out of the box.
+// Pos returns the position of a public key in keyset. Should the public key be
+// absent from the keyset, -1 is returned to indicate the key was not found.
 func (set PublicKeySet) Pos(key *PublicKey) int {
 	for p, v := range set {
 		if v.IsEqual(key) {
@@ -97,6 +89,7 @@ func (set PublicKeySet) ToStringArray() []string {
 	return rv
 }
 
+// Add appends a public key to the public key set, if it is not already present.
 func (set PublicKeySet) Add(key *PublicKey) PublicKeySet {
 	if set.Pos(key) < 0 {
 		set = append(set, *key)
@@ -104,7 +97,7 @@ func (set PublicKeySet) Add(key *PublicKey) PublicKeySet {
 	return set
 }
 
-// remove will remove the element at position i.
+// Remove removes a public key at a specified index from the public key set.
 func (set PublicKeySet) Remove(pos int) PublicKeySet {
 	if pos < 0 || pos >= len(set) {
 		return set
@@ -115,7 +108,8 @@ func (set PublicKeySet) Remove(pos int) PublicKeySet {
 	return set[:len(set)-1]
 }
 
-// Equal will compare two key sets.
+// Equal compares the public key set to the one passed, returning true if both
+// sets are equivalent.
 func (set PublicKeySet) Equal(v PublicKeySet) bool {
 	if set == nil && v == nil {
 		return true
